635fd415f7f6b9cf8fde1f1b87605e6066f9f0a7
newtoallofthis123 <noobscience123@gmail.com>
2023-12-01 15:24:44 +0530

    Fix Shadcn issue

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
@@ -44,6 +44,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
+      // @ts-ignore
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
