729ad46b5496400266814c99c4724b90330c6bc7
newtoallofthis123 <noobscience123@gmail.com>
2023-12-11 22:48:11 +0530

    Add sudo path

--- src/app/api/go/sudo/route.ts
+++ src/app/api/go/sudo/route.ts
@@ -1,20 +1,7 @@
 import { ranHash } from '@/lib/common';
 import { connectToDatabase, type Database } from '@/lib/db';
 
-export const GET = async (req: Request) => {
-    const {searchParams} = new URL(req.url)
-    const slug = searchParams.get('slug')
-    const { db }: { db: Database } = await connectToDatabase();
-    if (slug === null) {
-        return Response.json({ 
-            "error": "no slug provided"
-         });
-    } 
-    const data = await db.collection('go').findOne({slug});
-    return Response.json({ data });
-};
-
-export const POST = async (request: Request) => {
+export const POST = async (request: Request ) => {
     const data = await request.formData();
     const url = data.get('url');
     const slug = data.get('slug') || ranHash();
